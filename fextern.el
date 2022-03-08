e8f0739dcf56e75541ee2e8cf6d6a54b286a9e15
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-08 17:32:21 +0800

    Add src

---
+++ fextern.el
@@ -0,0 +1,38 @@
+;;; fextern.el --- Record file external states  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Created date 2022-03-08 17:30:07
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Record file external states.
+;; Keyword: externally file states
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/emacs-vs/fextern
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Record file external states.
+;;
+
+;;; Code:
+
+
+
+(provide 'fextern)
+;;; fextern.el ends here
